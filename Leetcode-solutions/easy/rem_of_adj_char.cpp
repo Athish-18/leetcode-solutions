7009ba498de6af5af76b0648a9064f1a78dbba4f
Athish <cathishk@gmail.com>
2025-07-02 23:00:40 +0530

    added code

---
+++ Leetcode-solutions/easy/rem_of_adj_char.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+  string removeDuplicates(string s)
+  {
+    int i = 0;
+    while (i < (int)s.length() - 1)
+    {
+      if (s[i] == s[i + 1])
+      {
+        s.erase(i, 2);
+        // Move back one step to re-check new neighbors
+        if (i > 0)
+          i--;
+      }
+      else
+      {
+        i++;
+      }
+    }
+    return s;
+  }
+};
