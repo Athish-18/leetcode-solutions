b4ee4be52bed10e54272a579c1ac4b83344a5ee1
Athish <cathishk@gmail.com>
2025-07-25 23:26:45 +0530

    added code

--- Leetcode-solutions/easy/2sum_not_optimised.cpp
+++ Leetcode-solutions/easy/2sum_not_optimised.cpp
@@ -17,6 +17,15 @@ public:
         }
       }
     }
-    return {}; // if no pair is found
+    return {}; 
   }
 };
+
+def twoSum(nums, target):
+    num_map = {}  # To store number and its index
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    return []  # If no solution (won't happen per problem statement)
