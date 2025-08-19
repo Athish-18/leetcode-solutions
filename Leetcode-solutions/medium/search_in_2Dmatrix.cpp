23ba515b19eabd4d57485fd790b6727ae5a8569d
Athish <cathishk@gmail.com>
2025-08-19 22:28:17 +0530

    added code

---
+++ Leetcode-solutions/medium/search_in_2Dmatrix.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+  bool searchMatrix(vector<vector<int>> &matrix, int target)
+  {
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+
+    int i = 0, j = cols - 1; // start from top-right
+
+    while (i < rows && j >= 0)
+    {
+      if (matrix[i][j] == target)
+      {
+        return true;
+      }
+      else if (matrix[i][j] > target)
+      {
+        j--; // move left
+      }
+      else
+      {
+        i++; // move down
+      }
+    }
+
+    return false;
+  }
+};
